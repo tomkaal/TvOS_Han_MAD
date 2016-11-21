0442e4feb9321b51f1d2b56a8fa408e1d7036b8c
Wesley Egbertsen <wesleyegbertsen@gmail.com>
2016-11-21 11:24:54 +0100

    refactored db model (Backend)

--- Backend/models/tv.js
+++ Backend/models/tv.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var tvSchema = new Schema({
-    description: {type: String}
+    description: {type: String},
+    beaconId: {type: String}
 });
 
 var Tv = mongoose.model('Tv', tvSchema, 'tvs');
