5c55c25c95f5b1aa076c076f0de1c6869f5cdf54
tomkaal <tommie_kaal@hotmail.com>
2016-11-21 12:16:48 +0100

    added loader vanishing on load

--- AndroidApp/MijnParkController/app/src/main/java/tvos/mad/han/mijnparkcontroller/JoinTeam.java
+++ AndroidApp/MijnParkController/app/src/main/java/tvos/mad/han/mijnparkcontroller/JoinTeam.java
@@ -1,10 +1,21 @@
 package tvos.mad.han.mijnparkcontroller;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class JoinTeam extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +27,23 @@ public class JoinTeam extends AppCompatActivity {
         String userName = extras.getString("name");
         String group = extras.getString("group");
 
+        progressBar = (ProgressBar) findViewById(R.id.teamProgressBar);
+        listView = (ListView) findViewById(R.id.teamListView);
+        ArrayList<String> values = new ArrayList<String>();
+        for (int i = 0; i < 15; i++) {
+            values.add("Team " + (i + 1));
+        }
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // Actions to do after 10 seconds
+                progressBar.setVisibility(View.GONE);
+                listView.setAdapter(adapter);
+
+            }
+        }, 3000);
     }
 }
