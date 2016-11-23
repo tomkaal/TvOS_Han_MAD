78cfc64a8549529e47b525323d5d08907ac71335
Wesley Egbertsen <wesleyegbertsen@gmail.com>
2016-11-23 10:46:32 +0100

    Implemented team post route for frontend starting game functionaility

--- Backend/controllers/teamController.js
+++ Backend/controllers/teamController.js
@@ -3,31 +3,61 @@
 
 var mongoose = require('mongoose'),
     Team = mongoose.model('Team'),
+    User = mongoose.model('User'),
     asyncEach = require('async/each');
 
-exports.createOne = function (req, res) {
-    //var doc = new Team(req.body);
+function sendErr(res, err) {
+    return res.send({
+        doc: null,
+        err: err
+    });
+}
+
+// Controller function that the frontend will use to start a game
+exports.createTeams = function (req, res) {
     var data = req.body;
 
+    var doc =  {
+        groupId: data.groupId,
+        teams: []
+    };
+
     asyncEach(data.teams,
-        function(team, callback) {
-            //do stuff to add team to db and users to that team then call callback
+        function(team, teamsCallback) {
+            var newTeam = new Team({
+                name: team.name,
+                group: data.groupId
+            });
+
+            newTeam.save(function (err) {
+                if (err) { return sendErr(res, err); }
+                var docTeam  = {
+                    teamId: newTeam._id,
+                    users: []
+                };
+                //update all users with their associated team
+                asyncEach(team.users,
+                    function(user, usersCallback) {
+                        User.update({ _id: user.id }, { $set: { team: newTeam._id }}, function (err) {
+                            //User updated, check for err
+                            if (err) { return sendErr(res, err); }
+                            docTeam.users.push({userId: user.id});
+                            usersCallback(); //tells the asyncEach function for users that the this iteration is done
+                        });
+                },
+                    function(err) {
+                        if (err) { return sendErr(res, err); }
+                        doc.teams.push(docTeam);
+                        teamsCallback(); //tells the asyncEach function for teams that the this iteration is done
+                });
+            });
         },
         function(err) {
-            //All teams are added and users update give response
-        });
-
-    doc.save(function (err) {
-        if (err) {
+            if (err) { return sendErr(res, err); }
+            //All teams are added and users are updated, give response
             return res.send({
-                doc: null,
+                doc: doc,
                 err: err
             });
-        }
-
-        res.send({
-            doc: doc,
-            err: err
         });
-    });
 };
