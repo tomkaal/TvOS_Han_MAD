3e826664be7e0d78538ea064572a1995d6263fad
Arthur van Rijsewijk <arthurvrijsewijk@gmail.com>
2016-11-29 09:59:39 +0100

    Added the ability to answer a question

--- Backend/controllers/questionController.js
+++ Backend/controllers/questionController.js
@@ -1,3 +1,22 @@
-/**
- * Created by Arthur on 21-Nov-16.
- */
+/*jslint node: true */
+"use strict";
+
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var Answer = mongoose.model('Answer');
+var Team = mongoose.model('Team');
+var Question = mongoose.model('Question');
+
+exports.answer = function (req, res) {
+    var quizId = req.body.quizId;
+    var questionId = req.body.questionId;
+    var userId = req.body.userId;
+
+    Quiz.findOne({ _id: quizId}, function(err, quiz){
+        Question.findOne($and [{ _id: questionId}, {quiz: quiz}], function (err, question){
+            User.findByIdAndUpdate(userId, { $push: { questions: [question]}}, { new: true }, function () {
+                return res.status(200).send();
+            });
+        });
+    });
+};
