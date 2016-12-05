38388b91d227af5ef524cf0a4d158668f4419d3d
Wesley Egbertsen <wesleyegbertsen@gmail.com>
2016-12-05 11:06:30 +0100

    Revert "Updated mongodb port to default port"
    This reverts commit bc9fa01f78a54e4612a071901dacd1948d8da89f.

--- Backend/config.js
+++ Backend/config.js
@@ -5,6 +5,6 @@ module.exports = {
     development: {
         debug: true,                                            // set debugging on/off
         db: 'mongodb://localhost/tvos',                         // change with your database
-        port: 27017                                              // change 3000 with your port number
+        port: 3000                                              // change 3000 with your port number
     }
 };
