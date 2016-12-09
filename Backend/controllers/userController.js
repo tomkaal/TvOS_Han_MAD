b6f79a031cd279003cc491952428717f1accc097
Arthur van Rijsewijk <arthurvrijsewijk@gmail.com>
2016-12-09 15:05:18 +0100

    added answer functionality for the user

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,8 +1,12 @@
 /*jslint node: true */
 "use strict";
 
-var mongoose = require('mongoose'),
-    User = mongoose.model('User');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var Answer = mongoose.model('Answer');
+var Team = mongoose.model('Team');
+var Question = mongoose.model('Question');
+var Quiz = mongoose.model('Quiz');
 
 exports.createOne = function (req, res) {
     var doc = new User(req.body);
@@ -18,6 +22,70 @@ exports.createOne = function (req, res) {
         res.send({
             doc: doc,
             err: err
+        });
+    });
+};
+
+
+
+exports.answer = function (req, res) {
+    var quizId = req.body.quizId;
+    var questionId = req.body.questionId;
+    var answerId = req.body.answerId;
+    var userId = req.body.userId;
+    var correct = false;
+    var allUsersAnswered = true;
+    var userQuestions = [];
+
+    // The quiz has the question with the correct answer
+    Quiz.findOne({ _id: quizId}, function(err, quiz){
+        // Find the question with the correct answer
+        Question.findOne($and [{ _id: questionId}, {quiz: quiz}], function (err, currentQuestion){
+            // Find the answer model the user has given
+            Answer.findOne($and [{ _id: answerId}, {quiz: quiz}], function (err, currentAnswer){
+                // Check if the answer the user has given is the correct answer
+                if(currentQuestion.correctAnswer == currentAnswer) {
+                    correct = true;
+                }
+                // Search for the current user and push the question in the questionarray
+                User.findByIdAndUpdate(userId, { $push: {questions: [currentQuestion, correct]} }, function (err, currentUser) {
+                    currentUser.populate('team');
+                    // Find the team, the user belongs to.
+                    Team.findById(currentUser.team._id, function (err, currentTeam){
+                        currentTeam.populate('users');
+                        // Loop through each user, to get all questions and add the most frequent question to the team
+                        currentTeam.users.forEach(function(teamUser) {
+                            teamUser.populate('questions');
+                            if(!teamUser.questions.contains(currentQuestion)) {
+                                allUsersAnswered = false;
+                            }
+                            teamUser.questions.forEach(function (teamUserQuestion) {
+                                if (teamUserQuestion == currentQuestion) {
+                                    userQuestions.push(teamUserQuestion);
+                                }
+                            });
+                        });
+
+                        if(allUsersAnswered) {
+                            // http://stackoverflow.com/questions/3783950/get-the-item-that-appears-the-most-times-in-an-array
+                            var frequency = {};  // array of frequency.
+                            var max = 0;  // holds the max frequency.
+                            var result;   // holds the max frequency element.
+                            for (var v in userQuestions) {
+                                frequency[userQuestions[v]] = (frequency[userQuestions[v]] || 0) + 1; // increment frequency.
+                                if (frequency[userQuestions[v]] > max) { // is this frequency > max so far ?
+                                    max = frequency[userQuestions[v]];  // update max.
+                                    result = userQuestions[v];          // update result.
+                                }
+                            }
+
+                            currentTeam.questions.add(result).save();
+                            return (res.json({doc: currentQuestion.score}))
+                        }
+                    });
+                    return res.status(200).send();
+                });
+            });
         });
     });
 };
